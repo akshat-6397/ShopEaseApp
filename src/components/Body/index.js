ed75ad3887f8062657559426c69fb3e89a2f8b3c
akshat-6397 <star5akshat@gmail.com>
2023-09-07 16:22:55 +0530

    body created

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -18,8 +18,16 @@ const Body = () => {
         <h1>Shop By Category</h1>
       </div>
       <hr className="hr-2" style={{ marginTop: "10px" }}></hr>
-      <div className="men-banner">
-        <img alt="men-banner" src="https://i.pinimg.com/originals/05/42/59/05425956e846b562dff86f712b70a6f6.png"/>
+      <div className="banners">
+        <div className="men-banner">
+            <img alt="men-banner" src="https://i.pinimg.com/originals/05/42/59/05425956e846b562dff86f712b70a6f6.png"/>
+        </div>
+        <div className="women-banner">
+            <img alt="women-banner" src="http://psdfreedownload.com/wp-content/uploads/2017/10/Free_Fashion_-Clothes_Banner_Psd_Full_Preview.jpg"/>
+        </div>
+        <div className="kids-banner">
+            <img alt="kids-banner" src="https://web-bazaar.netlify.app/static/media/kidsImage%20(1).7ae41c50045144004252.jpg"/>
+        </div>
       </div>
     </div>
   );
